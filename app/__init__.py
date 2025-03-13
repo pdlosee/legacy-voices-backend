20d54fe03dd6a9b6e1b8a0cf5223e92d0587ffb3
pdlosee <plosee@gmail.com>
2025-03-13 09:27:43 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ def create_app():
     app = Flask(__name__)
 
     # Enable CORS for all routes, restricted to your Netlify frontend URL
-   CORS(app, resources={r"/*": {"origins": ["https://your-personal-history.netlify.app", "http://localhost:3000", "http://127.0.0.1:5000"]}})
-
+    CORS(app, resources={r"/*": {"origins": ["https://your-personal-history.netlify.app", "http://localhost:3000", "http://127.0.0.1:5000"]}})
 
     from app.routes import bp as routes_bp
     app.register_blueprint(routes_bp)
