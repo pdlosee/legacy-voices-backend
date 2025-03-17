37e81d59020731ddd2d6f54e11f5edc14c62505f
pdlosee <plosee@gmail.com>
2025-03-16 18:19:46 -0600

    Delete app/db.py

--- app/db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-DATABASE = "legacy_voices.db"
-
-def get_db():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row  # Allows column access by name
-    return conn
